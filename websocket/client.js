ab96445a002644a8eda6d55576c9751c972fb80e
S. M. Abrar Jahin <abrarjahin@live.com>
2016-08-02 15:07:27 +0600

    JS Seperated

--- websocket/client.js
+++ websocket/client.js
@@ -1,10 +1,11 @@
+// Socket.io server listens to our app
 module.exports = function(io)
 {
-	// Emit welcome message on connection
-	io.on('connection', function(socket)
+	// Send current time to all connected clients
+	function sendTime()
 	{
-		// Use socket to communicate with this particular client only, sending it it's own id
-		socket.emit('welcome', { message: 'Welcome!', id: socket.id });
-		socket.on('i am client', console.log);
-	});
+		io.emit('time', { time: new Date().toJSON() });
+	}
+	// Send current time every 10 secs
+	setInterval(sendTime, 10000);
 }
--- websocket/server.js
+++ websocket/server.js
@@ -1,11 +1,20 @@
-// Socket.io server listens to our app
+// Socket.io server responce to our app
 module.exports = function(io)
 {
-	// Send current time to all connected clients
-	function sendTime()
+	// Emit welcome message on connection
+	io.on('connection', function(socket)
 	{
-		io.emit('time', { time: new Date().toJSON() });
-	}
-	// Send current time every 10 secs
-	setInterval(sendTime, 10000);
+		// Use socket to communicate with this particular client only, sending it it's own id
+		socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+		socket.on('i am client', console.log);
+	});
+
+	// io.on('connection', function(socket)
+	// {
+	// 	console.log('a user connected');
+	// 	socket.on('disconnect', function()
+	// 	{
+	// 		console.log('user disconnected');
+	// 	});
+	// });
 }
