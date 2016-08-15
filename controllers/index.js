6f3065c90981b25f27334a8761c9c03067dd222d
S. M. Abrar Jahin <abrarjahin@live.com>
2016-08-15 11:16:04 +0600

    Project Not Done - End Commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Post = require("../models/post");
+var Post = require("../models/post");		//Import Model
 
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
-	var user = new Post({
-							created_by: ""+Math.random()
-						}).save();
+	var post = new Post({created_by: ""+Math.random()});
 
-	res.render(
-				'index',
-				{
-					title		:	'Express',
-					site_name	:	'Our Site',
-					layout		:	'templates/layout'
-				}
-			);
+    post.save(function(error, data) {
+      if(error) {
+         return res.status(500).send({error: 'Error occurred during create post'});
+      }
+      return res.render('index',{
+          title       :   'Express',
+          site_name   :   'Our Site',
+          layout      :   'templates/layout'
+      });
+   });
+
+	// res.render(
+	// 				'index',
+	// 				{
+	// 					title		:	'Express',
+	// 					site_name	:	'Our Site',
+	// 					layout		:	'templates/layout'
+	// 				}
+	// 			);
 });
 
 router.get('/try', function(req, res, next)
